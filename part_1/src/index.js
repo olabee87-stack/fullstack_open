b2f0a00d637b5f6227d406c31c98570addbc0155
Olabisi <53159103+olabee87-stack@users.noreply.github.com>
2020-04-12 18:43:37 +0300

    created a practice component, Hello

--- part_1/src/index.js
+++ part_1/src/index.js
@@ -1,6 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+
+const Hello = (props) => {
+  return (
+    <div>
+      <p>
+        Hello {props.name}, ba wo {props.qua} {props.orisi} {props.age}
+      </p>
+    </div>
+  );
+};
+
+const App = () => {
+  const now = new Date();
+  const name = "Yemi";
+  const qua = "lovely";
+  const orisi = "babe";
+  const age = 20;
+  return (
+    <div className="app">
+      <h1>Greetings! It is {now.toDateString()}</h1>
+      <Hello name="Olabisi" qua="pretty" orisi="baby" age={age} />
+      <Hello name="Nathan" qua="handsome" orisi="boo" />
+      <Hello name={name} qua={qua} orisi={orisi} />
+      <Hello name={name} qua={qua} orisi={orisi} />
+    </div>
+  );
+};
 
 ReactDOM.render(<App />, document.getElementById("root"));
